6a0eb9a03fbc39b8e9568fb751b1e97c1e97f293
erendem <eren.demirdt@gmail.com>
2024-06-21 14:39:29 +0300

    back

--- bist_data.py
+++ bist_data.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def r_hello_world():
-    return 'Selam Yunus'
+    return jsonify({'msg': 'e'})
 
 
 if __name__ == '__main__':
